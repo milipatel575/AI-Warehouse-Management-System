8d739b067c85635014a9b2e1c3c7c75335646367
milipatel575 <milipatel575@gmail.com>
2026-08-09 20:06:37 +0530

    Prepare backend for deployment

--- config/db.js
+++ config/db.js
@@ -2,18 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Atlas Connected Successfully");
-
   } catch (error) {
-
-    console.error(
-      "MongoDB Atlas Connection Failed:",
-      error.message
-    );
-
+    console.error("MongoDB Atlas Connection Failed:", error.message);
     process.exit(1);
   }
 };
